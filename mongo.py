14f9f6587ae8fccc8335e9916dc53ae82f1930f8
Ubuntu <ubuntu@ip-172-31-84-217.ec2.internal>
2019-07-15 16:46:59 +0000

    Adding single entry

--- mongo.py
+++ mongo.py
@@ -17,6 +17,11 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
+new_doc = {'first': 'douglas', 'last': 'adams', 'dob':'11/03/1952', 
+            'hair_colour':'grey', 'occupation':'writer','nationality':'english'}
+            
+coll.insert(new_doc)
+
 documents = coll.find()
 
 for doc in documents:
